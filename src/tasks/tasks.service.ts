d659092756feedbece4d45070facc91886411257
akjha96 <akjha96@gmail.com>
2021-07-21 01:03:41 +0530

    Added Task Controller with GET & POST Rest API

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './tasks.model';
+import { v4 as uuid } from 'uuid';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks = (): Task[] => {
+    return this.tasks;
+  };
+
+  createTask = (title: string, description: string): Task => {
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.DONE,
+    };
+    this.tasks.push(task);
+    return task;
+  };
+}
