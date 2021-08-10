88c91e3d1299febe9710772e76a503bc5fb5ede7
akjha96 <akjha96@gmail.com>
2021-08-10 18:43:15 +0530

    removed unused TaskStatus  dependency from task.controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -11,7 +11,7 @@ import {
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
-import { Task, TaskStatus } from './tasks.model';
+import { Task } from './tasks.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
